7f3ce245ba35e06e4557d103a83e9e64a28a2ade
al16g20 <al16g20@soton.ac.uk>
2024-07-09 18:03:42 +0300

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import './App.css'
+import { Route, BrowserRouter as Router, Routes} from 'react-router-dom'
+import Navbar from './components/Navbar'
 
 function App() {
 
 
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-red-500">
-        Hello world!
-      </h1>
-    </div>
+    <main className='bg-slate-500/20'>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={'Home'}/>
+          <Route path='/about' element={'About'}/>
+          <Route path='/projects' element={'Projects'}/>
+          <Route path='/contact' element={'Contact'}/>
+        </Routes>
+      </Router>
+    </main>
   )
 }
 
