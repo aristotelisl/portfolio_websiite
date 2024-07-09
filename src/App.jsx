6b456090325429659ed1c61db1294db149979960
al16g20 <al16g20@soton.ac.uk>
2024-07-09 18:15:32 +0300

    page creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { Route, BrowserRouter as Router, Routes} from 'react-router-dom'
 import Navbar from './components/Navbar'
+import { Home, About, Projects, Contact } from './pages'
 
 function App() {
 
@@ -10,10 +11,10 @@ function App() {
       <Router>
         <Navbar/>
         <Routes>
-          <Route path='/' element={'Home'}/>
-          <Route path='/about' element={'About'}/>
-          <Route path='/projects' element={'Projects'}/>
-          <Route path='/contact' element={'Contact'}/>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/projects' element={<Projects/>}/>
+          <Route path='/contact' element={<Contact/>}/>
         </Routes>
       </Router>
     </main>
