0fb5d7a20c13c091639c463345f1e01791dc74a6
al16g20 <al16g20@soton.ac.uk>
2024-07-10 15:30:49 +0300

    3d model on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import Loader from '../components/Loader'
+import Butterfly from '../models/Butterfly'
 
 const Home = () => {
+
+  const adjustButterflyForScreenSize = () => {
+    let screenScale = null
+    let screenPosition = [0, 0, -30]
+    let rotation = [0.6, 0.05, -0.05]
+
+    if(window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9]
+    } else {
+      screenScale = [60, 60, 60]
+    }
+
+    return [screenScale, screenPosition, rotation]
+  }
+
+  const [butterflyScale, butterflyPosition, butterflyRotation] = adjustButterflyForScreenSize()
+
   return (
-    <div>Home</div>
+    <section className='w-full h-screen relative'>
+      <Canvas 
+        className='w-full h-screen relative'
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight/>
+          <ambientLight/>
+          <pointLight/>
+          <spotLight/>
+          <hemisphereLight/>
+          
+          <Butterfly 
+            position={butterflyPosition}
+            scale={butterflyScale}
+            rotation={butterflyRotation}
+          />
+        </Suspense>
+      </Canvas>
+    </section>
   )
 }
 
